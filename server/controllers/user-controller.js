f711dbb6dd10da1bedcf49f5acd372f19dd51363
EugeneDemidov <belopyckich@gmail.com>
2023-03-13 00:35:54 +0300

    written base request for login on server and create part. rewrite instances and added helpers for response

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,18 @@
 const userService = require("../service/user-service");
+const { validationResult } = require("express-validator");
+const ApiError = require("../exceptions/api-error");
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const validationResults = validationResult(req);
+
+      if (!validationResults.isEmpty()) {
+        return next(
+          ApiError.BadRequest("Ошибка при валидации", validationResults.array())
+        );
+      }
+
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
       res.cookie("refreshToken", userData.refreshToken, {
@@ -12,39 +22,68 @@ class UserController {
 
       return res.json(userData);
     } catch (error) {
-      console.log(error);
+      next(error);
     }
   }
 
   async login(req, res, next) {
     try {
-    } catch (error) {}
+      const { email, password } = req.body;
+      console.log(req.body, "req.body");
+      console.log(email, "email");
+      console.log(password, "password");
+      const userData = await userService.login(email, password);
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      next(error);
+    }
   }
 
   async logout(req, res, next) {
     try {
-    } catch (error) {}
-  }
-
-  async login(req, res, next) {
-    try {
-    } catch (error) {}
+      const { refreshToken } = req.cookies;
+      const token = await userService.logout(refreshToken);
+      res.clearCookie("refreshToken");
+      return res.json(token);
+    } catch (error) {
+      next(error);
+    }
   }
 
   async activate(req, res, next) {
     try {
-    } catch (error) {}
+      const activationLink = req.params.link;
+      await userService.activate(activationLink);
+      return res.redirect(process.env.CLIENT_URL);
+    } catch (error) {
+      next(error);
+    }
   }
 
   async refresh(req, res, next) {
     try {
-    } catch (error) {}
+      const userData = await userService.refresh(refreshToken);
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      next(error);
+    }
   }
 
   async getUsers(req, res, next) {
     try {
-      res.json(["123", "456"]);
-    } catch (error) {}
+      const users = await userService.getAllUsers();
+      return res.json(users);
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,13 +4,14 @@ const bcrypt = require("bcrypt");
 const uuid = require("uuid");
 const mailService = require("./mail-service");
 const tokenService = require("./token-service");
+const ApiError = require("../exceptions/api-error");
 
 class UserService {
   async registration(email, password) {
     const hasCandidateWithSameEmail = await UserModel.findOne({ email });
 
     if (hasCandidateWithSameEmail) {
-      throw new Error(
+      throw new ApiError.BadRequest(
         `Пользователь с почтовым адресом ${email} уже существует`
       );
     }
@@ -18,7 +19,12 @@ class UserService {
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4();
 
-    const user = await UserModel.create({ email, password: hashPassword });
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+
     await mailService.sendActivationMail(
       email,
       `${process.env.API_URL}/api/activate/${activationLink}`
@@ -29,6 +35,71 @@ class UserService {
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
     return { ...tokens, user: userDto };
+  }
+
+  async activate(activationLink) {
+    const user = await UserModel.findOne({ activationLink });
+
+    if (!user) {
+      throw new ApiError.BadRequest("Неккоректная ссылка активации");
+    }
+
+    user.isActivated = true;
+
+    await user.save();
+  }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({ email });
+    console.log(
+      "🚀 ~ file: user-service.js:54 ~ UserService ~ login ~ user:",
+      user
+    );
+
+    if (!user) {
+      throw ApiError.BadRequest("Пользователь с таким email не найден");
+    }
+
+    const isPassEquals = await bcrypt.compare(password, user.password);
+
+    if (!isPassEquals) {
+      throw ApiError.BadRequest("Неверный пароль");
+    }
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+    const userData = tokenService.validateRefreshToken({ refreshToken });
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError();
+    }
+    const user = await UserModel.findById(userData.id);
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async getAllUsers() {
+    const users = await UserModel.find();
+    return users;
   }
 }
 
